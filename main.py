14c5efe34bca6107fcaac0a089326ecedd7bf6ac
apiload5 <amir03182055632@gmail.com>
2025-02-04 01:09:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,41 @@
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.label import MDLabel
-from kivymd.uix.textfield import MDTextField
+from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.dialog import MDDialog
-
-import recoverpy
 
 KV = '''
 ScreenManager:
-    MainScreen:
-    RecoveryScreen:
+    HomeScreen:
 
-<MainScreen>:
-    name: 'main'
+<HomeScreen>:
+    name: 'home'
     MDBoxLayout:
         orientation: 'vertical'
-        padding: 20
-        spacing: 20
-
+        padding: dp(20)
+        spacing: dp(20)
         MDLabel:
-            text: 'Ticno Data Recovery'
+            text: 'Tecno Data Recovery'
             halign: 'center'
+            theme_text_color: 'Primary'
             font_style: 'H4'
-
-        MDRectangleFlatButton:
-            text: 'Recover Data'
-            on_release: root.manager.current = 'recovery'
-
-<RecoveryScreen>:
-    name: 'recovery'
-    MDBoxLayout:
-        orientation: 'vertical'
-        padding: 20
-        spacing: 20
-
-        MDLabel:
-            text: 'Recover Deleted Files'
-            halign: 'center'
-            font_style: 'H4'
-
-        MDTextField:
-            id: path_input
-            hint_text: 'Enter Path to Recover'
-
-        MDRectangleFlatButton:
-            text: 'Start Recovery'
-            on_release: root.start_recovery()
-
-        MDLabel:
-            id: status_label
-            text: ''
-            halign: 'center'
+        MDRaisedButton:
+            text: 'Start Data Recovery'
+            pos_hint: {'center_x': 0.5}
+            on_release: app.start_data_recovery()
 '''
 
-class MainScreen(Screen):
+class HomeScreen(Screen):
     pass
 
-class RecoveryScreen(Screen):
-    def start_recovery(self):
-        path = self.ids.path_input.text
-        if path:
-            try:
-                recoverpy.recover(path)
-                self.ids.status_label.text = 'Recovery Started!'
-            except Exception as e:
-                self.ids.status_label.text = f'Error: {str(e)}'
-        else:
-            self.ids.status_label.text = 'Please enter a valid path.'
-
-class TicnoDataRecoveryApp(MDApp):
+class TecnoDataRecoveryApp(MDApp):
     def build(self):
         return Builder.load_string(KV)
 
+    def start_data_recovery(self):
+        # Add your data recovery logic here
+        print("Data recovery started")
+
 if __name__ == '__main__':
-    TicnoDataRecoveryApp().run()
+    TecnoDataRecoveryApp().run()
